ddbf5904bff0a64a410d72078d8793d1288e992f
Kranti Nebhwani <darkphoton20@gmail.com>
2018-10-25 16:37:53 +0800

    Updated ai, security, and kiosk sections. Revamped scroll nav.

--- public/js/services.js
+++ public/js/services.js
@@ -4,23 +4,32 @@ function servNavScroll() {
     var breakPoint = 626;
     var servNav = document.getElementById('services-nav');
     var servNavList = document.querySelector('#services-nav ul.nav');
+    var servNavAnchor = document.querySelector('#services-nav ul.nav li a');
     window.addEventListener('scroll', function(e) {
         var scrollPosition = window.window.pageYOffset;
         if (scrollPosition > breakPoint) {
             servNav.classList.add('scroll-bar');
             servNavList.classList.add('scroll');
             document.querySelector('ul.nav li').innerHTML =
-                '<a href="#top"><i class="fas fa-arrow-up" /></a>';
+                '<a id="arrow" href="#top" style="color:#00fff6"><i class="fas fa-arrow-up" /></a>';
         }
         if (scrollPosition < breakPoint) {
             servNav.classList.remove('scroll-bar');
             servNavList.classList.remove('scroll');
 
-            document.querySelector('ul.nav li').innerHTML =
-                '<a href="#top">MENU</a>';
+            document.querySelector('ul.nav li').innerHTML = 'MENU |';
         }
-        console.log(scrollPosition);
+        // console.log(scrollPosition);
     });
+
+    // add / remove classes on arrow
+    servNavAnchor.addEventListener('mouseenter', function() {
+        console.log('test');
+        servNavAnchor.classList.add('mouse-over');
+    });
+    // servNavAnchor.addEventListener('mouseleave', function() {
+    //     sverNavAnchor.classList.remove('mouse-over');
+    // });
 }
 
 servNavScroll();
